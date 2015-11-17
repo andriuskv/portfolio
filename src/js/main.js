01c6af4856ca4872d7659932df6cd82ba1c7a166
Andrius Kvedaras <ikandrius@gmail.com>
2015-11-17 11:27:55 +0200

    Page redesign

--- src/js/main.js
+++ src/js/main.js
@@ -1,136 +1,69 @@
-/* global $ */
 "use strict";
 
 (function () {
-    var homeOffset = $("#home").offset().top,
-        workOffset = $("#work").offset().top,
-        contactOffset = $("#contact").offset().top,
-        currentSection = "",
+    var workOffset = document.getElementById("work").offsetTop,
+        homeOffset = document.getElementById("home").offsetTop,
+        nav = document.getElementById("js-nav-list"),
+        currentSection = "home",
         ticking = false;
     
-    function update() {
-        var currentPos = window.pageYOffset;
+    function isElementsBottomVisible(id) {
+        var element = document.getElementById(id);
         
+        return element.offsetTop + element.clientHeight <= document.body.clientHeight + document.documentElement.scrollTop;
+    }
+    
+    function setNavClass(classToAdd) {
+        nav.className = "nav-list " + classToAdd;
+    }
+    
+    function updateNav() {
+        var currentPos = window.pageYOffset;
+
         if (currentSection !== "home" && currentPos > homeOffset && currentPos < workOffset) {
-            toggleClass("#nav-home");
             currentSection = "home";
+            setNavClass("nav-default");
         }
-        else if (currentSection !== "work" && currentPos > workOffset - 1 && currentPos < contactOffset - 100) {
-            toggleClass("#nav-work");
+        else if (currentSection !== "work" && currentPos >= workOffset) {
             currentSection = "work";
+            setNavClass("nav-fixed");
         }
-        else if (currentSection !== "contact" && currentPos > contactOffset - 101) {
-            toggleClass("#nav-contact");
+        else if (currentSection !== "contact" && isElementsBottomVisible("contact")) {
             currentSection = "contact";
         }
         
-        toggleNavClass(currentSection);
         ticking = false;
-    }
-    
-    function toggleClass(id) {
-        $("#nav-home").removeClass().addClass("nav-item");
-        $("#nav-work").removeClass().addClass("nav-item");
-        $("#nav-contact").removeClass().addClass("nav-item");
-        $(id).addClass("active");
-    }
-    
-    function toggleNavClass(section) {
-        var nav = $("#js-nav");
-
-        nav.removeClass().addClass("nav");
-
-        if (section === "work" || section === "contact") {
-            nav.addClass("nav-fixed");
-        }
-        else {
-            nav.addClass("default");
-        }
     }
     
     function onScroll() {
         if (!ticking) {
-            requestAnimationFrame(update);
+            ticking = true;
+            requestAnimationFrame(updateNav);
         }
-        
-        ticking = true;
     }
+    
+    updateNav();
     
     window.addEventListener("scroll", onScroll, false);
-})();
 
-(function navigation() {
-    var $body = $("html, body"),
-        $window = $(window);
-
-    $(".nav-link").on("click", function(event) {
-        event.preventDefault();
-        $body.animate({
-            scrollTop: $($.attr(this, "href")).offset().top
-        }, 400);
-    });
-
-    $window.on("beforeunload", function() {
-        $window.scrollTop(0);
-    });
-})();
-
-(function() {
-    var $submit = $("#js-form-submit");
-
-    function toggleSubmitMsg() {
-        $("#js-form-submit-msg").toggle("show");
-        $submit.toggle("hide");
-    }
-    
-    function getForm(form) {
-        return {
-            name: form.name.value || "",
-            _replyto: form._replyto.value,
-            message: form.message.value
-        };
-    }
-    
-    function checkIfValid(form) {
-        var email = form._replyto.value;
-
-        return !!form.message.value && !!email && /@/.test(email);
-    }
-    
-    function sendMessage(form) {
-        $.ajax({
-            cache: false,
-            url: "http://formspree.io/ikandrius@gmail.com",
-            method: "POST",
-            data: getForm(form),
-            dataType: "json"
-        }).done(function() {
-            toggleSubmitMsg();
-
-            setTimeout(function() {
-                $submit.html("Send");
-                toggleSubmitMsg();
-            }, 5000);
-
-            if (form.reset) {
-                form.reset();
+    nav.addEventListener("click", function(event) {
+        var target = event.target,
+            section = "";
+        
+        if (target.className.indexOf("link-content") !== -1) {
+            section = target.innerHTML.toLowerCase();
+            
+            window.removeEventListener("scroll", onScroll, false);
+            
+            if (section === "home" && currentSection !== "home") {
+                setNavClass("nav-default");
             }
-        });
-    }
-    
-    function submitForm() {
-        var form = document.getElementById("form"),
-            valid = form.checkValidity ? form.checkValidity() : checkIfValid(form);
-
-        if (valid) {
-            $submit.html("Sending");
-            sendMessage(form);
-            return false;
+            else if ((section === "work" || section === "contact") &&
+                    (currentSection !== "work" || currentSection !== "contact")) {
+                setNavClass("nav-fixed");
+            }
+            
+            window.addEventListener("scroll", onScroll, false);
         }
-        else if (!form.checkValidity) {
-            return false;
-        }
-    }
-    
-    $submit.on("click", submitForm);
+    }, false);
 })();
