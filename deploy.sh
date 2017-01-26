fe85fbf8b60f11519ee8951dc05f654b282d3392
Andrius Kvedaras <ikandrius@gmail.com>
2017-01-26 11:49:08 +0200

    Add Travis CI

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+cd dist
+git init
+
+git config user.name "Andrius Kvedaras"
+git config user.email "ikandrius@gmail.com"
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+# Force push from the current repo's master branch to the remote repo's gh-pages branch.
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
